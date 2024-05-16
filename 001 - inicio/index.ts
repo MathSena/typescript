aad84102a5dc12d180c5981f8ef0afec6677c060
MathSena <MathSena07@hotmail.com>
2024-05-16 22:36:46 +0100

    Criando projeto do curso

---
+++ 001 - inicio/index.ts
@@ -0,0 +1,22 @@
+console.log('Instalando o TypeScript:')
+console.log('1. Certifique-se de ter o Node.js instalado no seu computador.')
+console.log(
+  "2. Abra o terminal e execute o comando 'npm install -g typescript' para instalar o TypeScript globalmente."
+)
+
+console.log('\nBaixando o TypeScript:')
+console.log(
+  '1. Acesse o site oficial do TypeScript em https://www.typescriptlang.org/'
+)
+console.log("2. Clique no botão 'Download' para baixar o pacote do TypeScript.")
+
+console.log('\nHistória do TypeScript:')
+console.log('O TypeScript foi desenvolvido pela Microsoft e lançado em 2012.')
+console.log(
+  'Ele é uma linguagem de programação de código aberto que estende o JavaScript, adicionando recursos como tipagem estática, classes e interfaces.'
+)
+console.log(
+  'O TypeScript foi projetado para melhorar a produtividade dos desenvolvedores e facilitar a manutenção de grandes projetos JavaScript.'
+)
+
+console.log('\nAgora você está pronto para começar a usar o TypeScript!')
